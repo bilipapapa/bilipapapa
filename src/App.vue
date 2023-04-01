180d0b4b06a8a4c85850a591a8fb34f6752d710c
wangjunpeng <18438622750@163.com>
2023-04-01 21:41:19 +0800

    feat:增加title-icon,首页背景

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,23 @@
 
 <script setup lang="ts"></script>
 
-<style scoped>
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100%;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
 
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: transparent url('@/assets/img/app_bg.png') no-repeat 0 0 / 100% 100%;
+    filter: blur(10px);
+    z-index: -1;
+    background-size: cover;
+  }
+}
 </style>
