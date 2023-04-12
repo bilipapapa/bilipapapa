c4664cb9cd8eaa17d86c850f0462ac371c33cfa7
wangjunnpeng <18438622750@163.com>
2023-04-12 16:07:07 +0800

    fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,10 @@
 import { obj } from './type'
-let file: any = import.meta.glob('./modules/*.ts', {
+let files: any = import.meta.glob('./modules/*.ts', {
   eager: true, // 立即导出(非异步方式)
-  import: 'default', // 默认值，不加时写法egg: file[path].default.$id，file[path].default
+  import: 'default', // 默认值，不加时写法egg: files[path].default.$id，files[path].default
 })
 const modules: obj = {}
-for (let path in file) {
-  modules[file[path]().$id] = file[path]()
+for (let path in files) {
+  modules[files[path]().$id] = files[path]()
 }
 export default modules
