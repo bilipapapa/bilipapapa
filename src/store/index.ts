8f58a803c44d635d5d5f3aac44254d19fa327433
wangjunnpeng <18438622750@163.com>
2023-04-12 16:05:50 +0800

    注释调整

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import { obj } from './type'
 let file: any = import.meta.glob('./modules/*.ts', {
-  eager: true, // 立即导出
+  eager: true, // 立即导出(非异步方式)
   import: 'default', // 默认值，不加时写法egg: file[path].default.$id，file[path].default
 })
 const modules: obj = {}
